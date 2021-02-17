f8678e080068bf39eaf7789ca7ed493a01f236d3
Ken Luu <ken.luuu@hotmail.com>
2021-02-16 22:10:38 -0500

    initial commit for bot

--- bot.py
+++ bot.py
@@ -1,1 +1,36 @@
-from dotenv import load_env
+from dotenv import load_dotenv
+from flask import Flask
+import os
+import slack
+from pathlib import Path
+
+# .env file from root
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+# api token
+token = os.getenv("BOT_TOKEN")
+
+# Creating flask app.
+app = Flask(__name__)
+
+print(token)
+# bot client
+bot = slack.WebClient(token=token)
+
+#random msg.
+bot.chat_postMessage(channel='#general', text="Hello World")
+
+@app.route("/")
+def root():
+    return "Hello World"
+
+@app.route("/test")
+def smthn():
+    return "Something else"
+
+# load Slack token
+token = os.getenv("BOT_TOKEN")
+
+if __name__ == "__main__":
+    app.run(debug=True)
