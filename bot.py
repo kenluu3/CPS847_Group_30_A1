4dd871dd172a0add8a43169ab49093591f2f9071
Ken Luu <ken.luuu@hotmail.com>
2021-02-17 21:13:54 -0500

    bot echos messages back now

--- bot.py
+++ bot.py
@@ -1,35 +1,41 @@
 from dotenv import load_dotenv
 from flask import Flask
 import os
-import slack
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
 from pathlib import Path
 
 # .env file from root
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-# api token
+# api tokens
 token = os.getenv("BOT_TOKEN")
+sign_in_sec = os.getenv("SIGN_IN_SECRET")
 
 # Creating flask app.
 app = Flask(__name__)
 
-# bot client
-bot = slack.WebClient(token=token)
+# events
+slack_events_adapter = SlackEventAdapter(sign_in_sec, "/bot/events", app)
 
-#random msg.
-bot.chat_postMessage(channel='#general', text="Hello World")
+# Slack Bot Info
+client = WebClient(token=token)
+BOT_ID = client.auth_test().get("user_id")
 
-@app.route("/")
-def root():
-    return "Hello World"
+# bot event listeners
+@slack_events_adapter.on("message")
+def message(payload): # echos the message back.
+    if payload == {}:
+        return
+    else:
+        data = payload.get("event")
+        if (data.get("user") != BOT_ID):
+            client.chat_postMessage(
+                channel="#assignment1",
+                text=data.get("text")
+            )
 
-@app.route("/test")
-def smthn():
-    return "Something else"
-
-# load Slack token
-token = os.getenv("BOT_TOKEN")
-
+# start app
 if __name__ == "__main__":
     app.run(debug=True)
